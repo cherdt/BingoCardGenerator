3b5ce105e3073e59d579caf123f4da61dc3faec5
Chris Herdt <cherdt@gmail.com>
2018-01-21 15:31:11 -0600

    Fix #12

--- images.js
+++ images.js
@@ -18,49 +18,28 @@ var BINGO = {
         }
     },
 
-    onebit: function (canvas) {
-        "use strict";
-        var imgData = canvas.getContext("2d").getImageData(0, 0, canvas.width, canvas.height);
-        var i, colorVal, red, green, blue;
-        for (i = 0; i < imgData.data.length; i = i + 4) {
-            if (((imgData.data[i] + imgData.data[i + 1] + imgData.data[i + 2]) / 3) > 127) {
-                colorVal = 255;
-            } else {
-                colorVal = 0;
-            }
-            red = i;
-            green = i + 1;
-            blue = i + 2;
-            imgData.data[red] = colorVal;
-            imgData.data[green] = colorVal;
-            imgData.data[blue] = colorVal;
-        }
-        canvas.getContext("2d").putImageData(imgData, 0, 0);
-    },
-
-
     loadImage: function (file) {
         "use strict";
         var self = this;
-        Promise.all([
-            createImageBitmap(file)
-        ]).then(function (sprites) {
-            var canvasElement = document.createElement("canvas");
-            canvasElement.width = sprites[0].width;
-            canvasElement.height = sprites[0].height;
-            canvasElement.getContext('2d').drawImage(sprites[0], 0, 0);
-            //self.onebit(canvasElement);
-            // enqueue the image onto the queue of images
-            self.images.push(canvasElement);
-            // add the canvas element to the page
-            document.getElementById("images").appendChild(canvasElement);
-        });
+        var reader = new FileReader();
+        var img = document.createElement("img");
+
+        img.file = file;
+        self.images.push(img);
+
+        reader.onload = (function (aImg) {
+            return function (e) {
+                aImg.src = e.target.result;
+            };
+        }(img));
+        reader.readAsDataURL(file);
     },
 
     shuffle: function () {
         "use strict";
         var counter = this.images.length;
-        var temp, index;
+        var temp;
+        var index;
 
         // While there are elements in the array
         while (counter > 0) {
@@ -80,7 +59,13 @@ var BINGO = {
 
     makeCard: function () {
         "use strict";
-        var i, j, card, row, cell, image;
+        var i;
+        var j;
+        var card;
+        var row;
+        var cell;
+        var image;
+
         this.shuffle();
         document.getElementById("bingo-header").textContent = document.getElementById("title").value;
         card = document.getElementsByTagName("tbody")[0];
