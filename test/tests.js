d06fa100b35fa86ae7e9c456c742612eac6c8322
Chris Herdt <cherdt@gmail.com>
2018-06-02 11:04:41 -0500

    Add qunit test

---
+++ test/tests.js
@@ -0,0 +1,21 @@
+removeEmptyElements = function (arr) {
+    "use strict";
+    var newArray = [], i = 0;
+    for (i = 0; i < arr.length; i += 1) {
+        if (arr[i]) {
+            newArray.push(arr[i]);
+        }
+    }
+    return newArray;
+}
+
+QUnit.test("RemoveEmptyElements", function(assert) {
+	"use strict";
+	var testInput = ",,,A,,B,,,C,";
+	var testArray = testInput.split(",")
+	var cleanedArray = removeEmptyElements(testArray);
+	assert.equal(cleanedArray.length, 3, "Array has 3 elements")
+    assert.ok(cleanedArray[0] === "A", "First element is A");
+    assert.ok(cleanedArray[1] === "B", "Second element is B");
+    assert.ok(cleanedArray[2] === "C", "Third element is C");
+});
