330063196cdf675dc3728b31e413ed72b22b8052
Duncan Stuart <dgmstuart@gmail.com>
2020-05-24 01:17:52 +0200

    Make javascript testable
    The goal here is to have the tests be able to test the javascript code
    which is used in the page.
    
    In order to do that we need to extract the javascript out to a separate
    file so that the the QUnit tests can test it.
    
    This approach follows the advice in the qunit docs:
    https://qunitjs.com/intro/#make-things-testable
    
    ...but modified to allow running via the command line (and therefore on
    Travis) by following this stackoverflow post:
    https://stackoverflow.com/a/51861149

--- test/tests.js
+++ test/tests.js
@@ -1,19 +1,10 @@
-removeEmptyElements = function (arr) {
-    "use strict";
-    var newArray = [], i = 0;
-    for (i = 0; i < arr.length; i += 1) {
-        if (arr[i]) {
-            newArray.push(arr[i]);
-        }
-    }
-    return newArray;
-};
+bingoGenerator = require('../bingo_generator.js').bingoGenerator;
 
 QUnit.test("RemoveEmptyElements", function(assert) {
     "use strict";
     var testInput = ",,,A,,B,,,C,";
     var testArray = testInput.split(",");
-    var cleanedArray = removeEmptyElements(testArray);
+    var cleanedArray = bingoGenerator.removeEmptyElements(testArray);
     assert.equal(cleanedArray.length, 3, "Array has 3 elements");
     assert.equal(cleanedArray[0], "A", "First element is A");
     assert.equal(cleanedArray[1], "B", "Second element is B");
